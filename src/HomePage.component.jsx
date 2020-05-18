1780785c699658a9a3c3429caf78700c794ef0d7
Roshan <ravi.roshan04@gmail.com>
2020-05-18 21:27:33 +0530

    created block and added node-sass

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './HomePage.styles.scss';
+
+const HomePage = () => (
+	<div className='homepage'>
+		<h1>Welcome to my homepage</h1>
+		<div className='directory-menu'>
+			<div className='menu-item'>
+				<div className='content'>
+					<div className='title'>HATS</div>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<div className='title'>HATS</div>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<div className='title'>HATS</div>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<div className='title'>HATS</div>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<div className='title'>HATS</div>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default HomePage;
